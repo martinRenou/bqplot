b9663809aba6ff3084907c6b132914254b38e1ea
martinRenou <martin.renou@gmail.com>
2019-04-30 11:39:13 +0200

    Fragmentshader: Compute fill color only if fill == True
    Note: `if (fill)` could be replaced by `#if fill` ?

--- js/shaders/scatter-fragment.glsl
+++ js/shaders/scatter-fragment.glsl
@@ -33,22 +33,24 @@ vec2 rotate_xy(vec2 x, float angle) {
 
 void main(void) {
     vec2 pixel = (vUv - 0.5) * (pixel_size + 2.0 * stroke_width);
+    float fill_weight = 0.0;
 
 #if FAST_DRAW == FAST_CIRCLE
-    float r = length(pixel);
-    float fill_weight = 1.0 - smoothstep(pixel_size/2.0 - stroke_width - SMOOTH_PIXELS, pixel_size/2.0 - stroke_width + SMOOTH_PIXELS, r);
+    float r = length(pixel); // This uses `sqrt` which is slow, there might be room for performance improvements
+
+    fill_weight = 1.0 - smoothstep(pixel_size/2.0 - stroke_width - SMOOTH_PIXELS, pixel_size/2.0 - stroke_width + SMOOTH_PIXELS, r);
+
     float edge_weight = 1.0 - smoothstep(pixel_size/2.0 + stroke_width - SMOOTH_PIXELS, pixel_size/2.0 + stroke_width + SMOOTH_PIXELS, r);
     float stroke_weight = (1.0 - fill_weight) * edge_weight;
 
-    fill_weight *= (fill ? 1.0 : 0.0);
-
 #elif FAST_DRAW == FAST_SQUARE
-    float stroke_weight_inverse = smoothstep(-SMOOTH_PIXELS, SMOOTH_PIXELS, -(abs(pixel.x) - pixel_size/2.0 + stroke_width/2.0))
-                                * smoothstep(-SMOOTH_PIXELS, SMOOTH_PIXELS, -(abs(pixel.y) - pixel_size/2.0 + stroke_width/2.0));
-    float stroke_weight = 1.0 - stroke_weight_inverse;
-    float fill_weight =  1.0 * smoothstep(-SMOOTH_PIXELS, SMOOTH_PIXELS, -(abs(pixel.x) - pixel_size/2.0 + stroke_width/2.0))
-                             * smoothstep(-SMOOTH_PIXELS, SMOOTH_PIXELS, -(abs(pixel.y) - pixel_size/2.0 + stroke_width/2.0));
-    fill_weight *= (fill ? 1.0 : 0.0);
+    if (fill) {
+        fill_weight =  smoothstep(-SMOOTH_PIXELS, SMOOTH_PIXELS, -(abs(pixel.x) - pixel_size/2.0 + stroke_width/2.0)) *
+                       smoothstep(-SMOOTH_PIXELS, SMOOTH_PIXELS, -(abs(pixel.y) - pixel_size/2.0 + stroke_width/2.0));
+    }
+
+    float stroke_weight = 1.0 - smoothstep(-SMOOTH_PIXELS, SMOOTH_PIXELS, -(abs(pixel.x) - pixel_size/2.0 + stroke_width/2.0)) *
+                                smoothstep(-SMOOTH_PIXELS, SMOOTH_PIXELS, -(abs(pixel.y) - pixel_size/2.0 + stroke_width/2.0));
 
 #elif FAST_DRAW == FAST_ARROW
     // take 2 rotated coordinate systems
@@ -56,15 +58,17 @@ void main(void) {
     vec2 pixel_left = (rotate_xy(vUv - vec2(0.5, 1.0), -angle) + vec2(0.0, 0.5)) * (pixel_size + 2.0 * stroke_width);
     vec2 pixel_right = (rotate_xy(vUv - 1.0, angle) + 0.5) * (pixel_size + 2.0 * stroke_width);
 
+    if (fill) {
+        fill_weight = smoothstep(-SMOOTH_PIXELS, SMOOTH_PIXELS, pixel.y + pixel_size/2.0 - stroke_width/2.) *
+                      smoothstep(-SMOOTH_PIXELS, SMOOTH_PIXELS, pixel_left.x - stroke_width/2.) *
+                      smoothstep(-SMOOTH_PIXELS, SMOOTH_PIXELS, -pixel_right.x - stroke_width/2.);
+    }
+
     float bottom_width = tan(angle) * pixel_size;
     float edge_weight_bottom = (1.0 - smoothstep(-SMOOTH_PIXELS, SMOOTH_PIXELS, abs(pixel.y + pixel_size/2.0) - stroke_width/2.0))
                              * (1.0 - smoothstep(bottom_width - SMOOTH_PIXELS, bottom_width + SMOOTH_PIXELS, abs(pixel.x)));
     float edge_weight_left  = (1.0 - smoothstep(-SMOOTH_PIXELS, SMOOTH_PIXELS, abs(pixel_left.x) - stroke_width/2.0));
     float edge_weight_right = (1.0 - smoothstep(-SMOOTH_PIXELS, SMOOTH_PIXELS, abs(pixel_right.x) - stroke_width/2.0));
-
-    float fill_weight = smoothstep(-SMOOTH_PIXELS, SMOOTH_PIXELS, pixel.y + pixel_size/2.0 - stroke_width/2.)
-                      * smoothstep(-SMOOTH_PIXELS, SMOOTH_PIXELS, pixel_left.x  - stroke_width/2.)
-                      * smoothstep(-SMOOTH_PIXELS, SMOOTH_PIXELS, -pixel_right.x - stroke_width/2.);
 
     float stroke_weight = 1.0 - (1.0 - edge_weight_bottom) * (1.0 - edge_weight_left) * (1.0 - edge_weight_right);
 #endif
